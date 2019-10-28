b59023ee4c1f1766e1feac29a630069d71cf97b0
chougousui <chougousui@163.com>
2019-10-28 11:01:32 +0900

    Update init-funcs.el

--- lisp/init-funcs.el
+++ lisp/init-funcs.el
@@ -14,4 +14,11 @@
             ))
   )
 
+;; 切换到刚才打开过的buffer
+(defun choug/goto-last-buffer ()
+  "Switch to previously open buffer.
+Repeated invocations toggle between the two most recently open buffers."
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
+
 (provide 'init-funcs)
