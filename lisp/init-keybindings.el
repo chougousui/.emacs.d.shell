de7673ac87fad23e8ac8c6477dedfeaabea4b771
chougousui <chougousui@163.com>
2019-10-28 10:31:53 +0900

    company候选词选择快捷键

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -38,4 +38,11 @@
 ;; 不会与M-;冲突
 (global-set-key (kbd "C-c c l") 'comment-line)
 
+;; company候选词选择
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
 (provide 'init-keybindings)
